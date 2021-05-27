363eafc8a39e4c4b0dae4bd3d7e870378fc3c529
centtravy@gmail.com <centtravy@gmail.com>
2021-05-27 17:30:13 +0300

    Added Allowed Hosts

--- mtnpulse/settings.py
+++ mtnpulse/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-%@7a7j-41an3us9y-44^nfc*el4s^21-=fvf2_9i-b6(bb3(h^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mtnpulse.herokuapp.com']
 
 
 # Application definition
